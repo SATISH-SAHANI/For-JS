b1fe2cdbca5213f21ba0cd5ac74e71694d8b2fa5
satishsahani <satishsahani743@gmail.com>
2021-11-30 12:06:15 +0530

    check1

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,14 @@
 function CreateObject(arr) {
-    // Write your code here
+
+    obj = {};
+    for (let i = 1; i < arr.length; i += 2) {
+
+        if (obj[arr[i - 1]] === undefined) {
+            obj[arr[i - 1]] = arr[i];
+        }
+    }
+    return obj
+
 }
 
 module.exports = CreateObject;
